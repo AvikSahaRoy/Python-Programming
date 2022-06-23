e88b9f4f38ef4446e77d9e05bc7063d778b547ed
Avik Saha Roy <97282235+AvikSahaRoy@users.noreply.github.com>
2022-06-23 12:40:37 +0530

    Update Telephone.py

--- Telephone.py
+++ Telephone.py
@@ -1,13 +1,4 @@
 n = input("Enter your number: ")
-# a = ['a', 'b', 'c']
-# b = ['d', 'e', 'f']
-# c = ['g', 'h', 'i']
-# d = ['j', 'k', 'l']
-# e = ['m', 'n', 'o']
-# f = ['p', 'q', 'r', 's']
-# g = ['t', 'u', 'v']
-# h = ['w', 'x', 'y', 'z']
-
 a = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l'],['m', 'n', 'o'], ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']]
 
 new = []
@@ -38,40 +29,3 @@ else:
         for j in range(len(new[1])):
             new2.append(new[0][i]+new[1][j])
     print(new2) 
-
-
-# combinations = []
-
-# def combine(terms, accum):
-#     last = (len(terms) == 1)
-#     n = len(terms[0])
-#     for i in range(n):
-#         item = accum + terms[0][i]
-#         if last:
-#             combinations.append(item)
-#         else:
-#             combine(terms[1:], item)
-
-# combine(new, '')
-# print(combinations)
-
-
-
-# -------------------------------------------
-# combinations = []
-
-# def combine(terms, accum):
-#     last = (len(terms) == 1)
-#     n = len(terms[0])
-#     for i in range(n):
-#         item = accum + terms[0][i]
-#         if last:
-#             combinations.append(item)
-#         else:
-#             combine(terms[1:], item)
-
-
-# a = [['ab','cd','ef'],['12','34','56'],['x', 'y', 'z', '4']]
-# combine(a, '')
-# print(combinations)
-# ['ab12', 'ab34', 'ab56', 'cd12', 'cd34', 'cd56', 'ef12', 'ef34', 'ef56']
