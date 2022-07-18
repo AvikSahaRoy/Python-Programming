cf5cc1f904106f9a84470852fe1d1e245ce0f302
Avik Saha Roy <97282235+AvikSahaRoy@users.noreply.github.com>
2022-07-18 17:50:46 +0530

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -1,35 +1,10 @@
-# def main():
-#     count = 0
-#     n = int(input("Enter a number: "))
-#     for i in range(2,n):
-#         if(n%i == 0):
-#             count += 1
-#     if(count == 0):
-#         print("Prime")
-#     else:
-#         print('Not prime')
-# if __name__ == '__main__':
-#     main()
-
-# -----------------------------------------
-
-# class prime():
-#     def __init__(self, num):
-#         self.num = num
-#     def print_data(self):
-#         n = self.num
-#         count = 0
-#         for i in range(2,n//2):
-#             if(n%i == 0):
-#                 count += 1
-#         if(count == 0):
-#             print("Prime")
-#         else:
-#             print('Not prime')
-# if __name__ == "__main__":
-#     num = int(input("Enter a number: "))
-#     obj = prime(num)
-#     obj.print_data()
-
-
-
+# Print Prime numbers ---------->>>>>>>>
+n = int(input("Enter a number: "))
+flag = 0
+for i in range(2, n):
+    for j in range(2, i):
+        if (i%j == 0):
+            flag = 1
+    if flag == 0:
+        print(i)
+    flag = 0
