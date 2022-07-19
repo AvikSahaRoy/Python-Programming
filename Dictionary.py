70ad258fb1db7e2fb21254d01fb2b6acbbb9ddd4
Avik Saha Roy <97282235+AvikSahaRoy@users.noreply.github.com>
2022-07-19 08:37:12 +0530

    Add files via upload

--- Dictionary.py
+++ Dictionary.py
@@ -1,16 +1,11 @@
-# Dictionary : ---------------------
-d1 = {
-    4: 'Avipsa',
-    3: 'Soumo',
-    1: 'Tanu',
-    2: 'Asmita',
-    5: 'Gopal',
-    0: 'Avik'
-}
-print("Print Keys: ",d1.keys()) # dict_keys([4, 3, 1, 2, 5, 0])
-print("Print Values: ",d1.values()) # dict_values(['Avipsa', 'Soumo', 'Tanu', 'Asmita', 'Gopal', 'Avik'])
-print("Print Sorted Order: ",sorted(d1)) # [0, 1, 2, 3, 4, 5]
-for i in sorted(d1):
-    print(i,':',d1[i])
-
-
+n = 'institute'
+v = ['a', 'e', 'i', 'o', 'u']
+dict = {}
+for i in n:
+    if i in v:
+        keys = dict.keys()
+        if i in keys:
+            dict[i]+= 1
+        else:
+            dict[i] = 1
+print(dict)
